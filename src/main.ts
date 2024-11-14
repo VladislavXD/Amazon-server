2972b245646cd269b749193ea24457387750f0b4
VladislavXD <vladgamer1175@gmail.com>
2024-11-14 22:54:29 +0500

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
 
 
   
-  app.enableCors()
+  app.enableCors({
+    origin: 'the-amazon.vercel.app/', // укажите ваш домен Next.js
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Access-Control-Allow-Origin', // добавьте необходимые заголовки
+    credentials: true, // если нужно передавать cookies
+  });
   app.setGlobalPrefix('api')
   await app.listen(3500); 
 }
