c353d28ef944646b47c2a9017561a98c2e61ae19
Neivo <126583090+VladislavXD@users.noreply.github.com>
2024-11-14 18:44:43 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
 
 
   
-  app.enableCors()
+   app.enableCors({
+    origin: 'https://amazon-frontend.vercel.app', // укажите ваш домен Next.js
+    methods: 'GET, POST, PUT, DELETE', 'PATCH'
+    allowedHeaders: 'Content-Type, Authorization', // добавьте необходимые заголовки
+    credentials: true, // если нужно передавать cookies
+  });
   app.setGlobalPrefix('api')
   await app.listen(3500); 
 }
