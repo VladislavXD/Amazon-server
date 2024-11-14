b423a256e10269670a649f880ab3e27d2a404eba
Neivo <126583090+VladislavXD@users.noreply.github.com>
2024-11-14 22:30:28 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   
     app.enableCors({
-    origin: 'https://amazon-frontend.vercel.app', // укажите ваш домен Next.js
+    origin: 'the-amazon.vercel.app/', // укажите ваш домен Next.js
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials', // добавьте необходимые заголовки
     credentials: true, // если нужно передавать cookies
