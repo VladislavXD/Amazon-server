896708be316cf46806738a0133a7c2da83d0a63a
Neivo <126583090+VladislavXD@users.noreply.github.com>
2024-11-15 20:01:31 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
     app.enableCors({
     origin: '*', // ваш домен Next.js с https://
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization', // добавьте нужные заголовки, такие как Content-Type
     credentials: true, // если требуется передавать cookies
   });
   app.setGlobalPrefix('api')
