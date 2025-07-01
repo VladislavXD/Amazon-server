4f9685efaf77161d3be38dcdf35dd413c40b9b28
Vladislav <neivo@vladislav.dev>
2025-07-01 19:57:33 +0500

    update

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,30 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
 
 
   
   app.enableCors({
-    origin: 'the-amazon.vercel.app/', // укажите ваш домен Next.js
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Access-Control-Allow-Origin', // добавьте необходимые заголовки
-    credentials: true, // если нужно передавать cookies
+    origin: [
+      'http://localhost:3000',
+      'localhost:3000',
+      'https://the-amazon.vercel.app',
+      'http://the-amazon.vercel.app',
+      'https://localhost:3000',
+      'the-amazon.vercel.app'
+            ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization', 
+    credentials: true, 
   });
+
   app.setGlobalPrefix('api')
-  await app.listen(3500); 
+  
+  // Use dynamic port for Vercel deployment
+  const port = process.env.PORT || 3500;
+  await app.listen(port); 
+  
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
- 
